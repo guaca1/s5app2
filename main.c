78de02434887d107fcfb2b398abe3453b5007160
guaca1 <felix.landry.forget@usherbrooke.ca>
2017-09-11 10:52:04 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 #include "liste.h"
+#include "operande.h"
 
 //Fonction Asm
 void TaMEre();
@@ -15,13 +16,11 @@ int main(void) {
 	int liste[3];
     obtenirType(&liste[0]);
     obtenirFormat(&liste[1]);
-    obtenirOperation(&liste[2]);
-    int value = convertirListe(&liste[0], &liste[1]);
+    analyserListe(liste);
 
-/*    printf("%d", value);
     printf("%d", liste[0]);
     printf("%d", liste[1]);
-    printf("%d", liste[2]);*/
+    printf("%d", liste[2]);
 
 	return 0;
 }
